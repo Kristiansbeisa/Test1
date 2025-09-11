56400fb8841ddf94438b6ab92c0e234fe2e58409
Kristiansbeisa <kristiansbeisa@gmail.com>
2025-09-11 11:20:24 +0300

    Update Game.js

--- Game.js
+++ Game.js
@@ -342,7 +342,7 @@ function move() {
     if (checkSlowZone(left, top)) {
     step = slowSpeed; // lieto palēninātu ātrumu
 } else {
-    step = 10;
+    step = 3;
 }
 
     object.style.left = newLeft + 'px';
@@ -433,4 +433,5 @@ function addButtonListeners(buttonId, key) {
 addButtonListeners('upButton', 'ArrowUp');
 addButtonListeners('leftButton', 'ArrowLeft');
 addButtonListeners('downButton', 'ArrowDown');
+
 addButtonListeners('rightButton', 'ArrowRight');
